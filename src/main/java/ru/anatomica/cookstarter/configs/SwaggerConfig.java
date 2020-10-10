da4aec46b85ebc27568fba7746e37bf13dcaeb60
Maxim <maksfomin@gmail.com>
2020-10-11 01:55:46 +0300

    Token live & SwaggerConfig

--- src/main/java/ru/anatomica/cookstarter/configs/SwaggerConfig.java
+++ src/main/java/ru/anatomica/cookstarter/configs/SwaggerConfig.java
@@ -17,6 +17,7 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("ru.anatomica.cookstarter.controllers"))
                 .paths(PathSelectors.regex("/api.*"))
+                .paths(PathSelectors.regex("/auth"))
                 .build();
     }
 }
