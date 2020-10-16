90b000f2c1ecf9b40f36515c444b8e0ef4c71e3e
Maxim <maksfomin@gmail.com>
2020-10-16 19:19:03 +0300

    Repair project v18

--- src/main/java/ru/anatomica/cookstarter/configs/AppConfig.java
+++ src/main/java/ru/anatomica/cookstarter/configs/AppConfig.java
@@ -35,9 +35,9 @@ public class AppConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE")
-                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers")
-                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
-                .allowCredentials(true).maxAge(3600);
+                .allowedHeaders("Content-Type", "X-Requested-With", "Access-Control-Allow-Origin", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 
 }
